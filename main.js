e91307f230144c4372d0bc330e988f5670595d61
Arpan jain <167850968+Arpan-jain001@users.noreply.github.com>
2025-01-23 23:15:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,33 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
 });
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Form ko submit hone se roken
+    var submitButton = document.getElementById('submitButton');
+    var loadingIndicator = document.getElementById('loading');
+
+    // Button ko disable karein aur loading spinner dikhayein
+
+
+    // Form ko submit karein
+    this.submit();
+
+    // Form ko reset karein
+    this.reset();
+});
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Form ko submit hone se roken
+    var submitButton = document.getElementById('submitButton');
+    var loadingIndicator = document.getElementById('loading');
+
+    // Button ko disable karein aur loading spinner dikhayein
+    submitButton.disabled = true;
+    loadingIndicator.style.display = 'inline-block'; // Spinner ko dikhayein
+
+    // Form ko submit karein
+    this.submit();
+
+    // Form ko reset karein
+    this.reset();
+});
